f4ee1d46d4461e73978746dc501bd181306e2b30
Anner Escobar <70228927+AnnerEscobar@users.noreply.github.com>
2024-08-17 15:33:22 -0600

    Subiendo cambios

--- src/sgic-data/sgic-data.service.ts
+++ src/sgic-data/sgic-data.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateSgicCaso } from './dto/create-sgic-caso.dto';
 import { UpdateSgicDatumDto } from './dto/update-sgic-datum.dto';
 import { InjectModel } from '@nestjs/mongoose';
@@ -14,11 +14,19 @@ export class SgicDataService {
 
   ) { }
 
-
-  create(createSgicCaso: CreateSgicCaso) {
-    console.log(createSgicCaso);
-    return 'This action adds a new sgicDatum';
+  async create(createSgicCaso: CreateSgicCaso): Promise<SgicCaso> {
+    try {
+      const newCaso = new this.casosModel(createSgicCaso);
+      return await newCaso.save();
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(`${createSgicCaso.deicNumber} alredy exists !!!`)
+      }
+      throw new InternalServerErrorException('Something bad happened !!!')
+    }
   }
+
+
 
   findAll() {
     return `This action returns all sgicData`;

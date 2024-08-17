1f9f6eb27963751eca5d68df7d449ec3d1233ea1
Anner Escobar <70228927+AnnerEscobar@users.noreply.github.com>
2024-08-17 10:17:42 -0600

    conectando a base de datos y config variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
   import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { SgicDataModule } from './sgic-data/sgic-data.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
   @Module({
-    imports: [AuthModule, SgicDataModule],
+    imports: [
+      ConfigModule.forRoot(),
+      MongooseModule.forRoot(process.env.MONGO_URI),
+      AuthModule, SgicDataModule,],
     controllers: [],
     providers: [],
   })
-  export class AppModule {}
+  export class AppModule {
+
+    
+
+
+  }
 
   
